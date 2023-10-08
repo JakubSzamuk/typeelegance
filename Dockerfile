ba9a41ebd6ea4630cccdf944458a9a4db3ea2f45
Jakub Szamuk <lavaproofJakub1@gmail.com>
2023-10-08 20:56:06 +0100

    fix build

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ WORKDIR /app
 ADD / /app
 
 RUN yarn install --production
+RUN yarn add tailwindcss@latest postcss@latest autoprefixer@latest
 
 RUN yarn build
 EXPOSE 3000
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'TypeElegance',
@@ -21,7 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }

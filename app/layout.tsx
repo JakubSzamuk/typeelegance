9a554c4314f9ebaeceedc2f6c1a5a279d202f285
Jakub Szamuk <lavaproofJakub1@gmail.com>
2023-10-08 14:32:31 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>{children}</body>
